5b1fdc1917facf0e7b5ffadf9a7159ab08ba63ec
Benjamin Canou <canou@pps.jussieu.fr>
2009-05-01 14:09:25 +0200

    Empty log message
    Ignore-this: da53f2b6eb2d66666b9aa18f7f13f498
    
    darcs-hash:20090501120925-fadb5-8fdc141c4cb2b4c08c4e499d9614291d599db5f4

--- examples/test_vincent/table.ml
+++ examples/test_vincent/table.ml
@@ -1,15 +1,19 @@
-external register_closure
-  : int -> (unit -> unit) -> unit
-  = "caml_register_closure"
+open Js
 
-external get_closure_arg
-  : unit -> 'a
-  = "caml_get_closure_arg"
+let register_closure : int -> (unit -> unit) -> unit =
+  match js_external "caml_register_closure" 2 with
+    | None -> failwith "unbound external"
+    | Some f -> f
+
+let get_closure_arg : unit -> Obj.t =
+  match js_external "caml_get_closure_arg" 1 with
+    | None -> failwith "unbound external"
+    | Some f -> f
 
 let register_closure id f =
   register_closure id (fun () ->
 			 try
-			   ignore (f (get_closure_arg ())) ;
+			   ignore (f (Obj.obj (get_closure_arg ()))) ;
 			   Thread.exit ()
 			 with _ ->
 			   Thread.exit ())
