3f38da631233f958e7d879046238854318dba6e4
Benjamin.Canou <Benjamin.Canou@gmail.com>
2009-06-12 16:17:53 +0200

    frames update
    darcs-hash:20090612141753-2ddb7-e3c02b8b2f75ca7294c2acceb3ab8aad3e935720

--- examples/frames/style.ml
+++ examples/frames/style.ml
@@ -1,15 +1,32 @@
 open JSOO
 
 type position = [ `ABSOLUTE | `FIXED | `RELATIVE ]
+
 type dim = [ `PX of int | `EX of int | `EM of int | `PCT of int ]
 let px x = `PX x
 let ex x = `EX x
 let em x = `EM x
 let pct x = `PCT x
 
+type color = string
+
 class style obj =
 object
   val sty = obj >>> get "style"
+
+  (* generic methods *)
+
+  method set_dim (n : string) (v : dim) : unit =
+    sty >>> set n
+      (match v with
+	 | `PX v -> string (string_of_int v ^ "px")
+	 | `EX v -> string (string_of_int v ^ "px")
+	 | `EM v -> string (string_of_int v ^ "px")
+	 | `PCT v -> string (string_of_int v ^ "%")
+      )
+
+  (* properties *)
+
   method position : position =
     match sty >>> get "position" >>> as_string with
       | "fixed" -> `FIXED
@@ -21,15 +38,17 @@ object
       | `FIXED -> sty >>> set "position" (string "fixed")
       | `RELATIVE -> sty >>> set "position" (string "relative")
       | `ABSOLUTE -> sty >>> set "position" (string "absolute")
-  method set_dim (n : string) (v : dim) : unit =
-    sty >>> set n
-      (match v with
-	 | `PX v -> string (string_of_int v ^ "px")
-	 | `EX v -> string (string_of_int v ^ "px")
-	 | `EM v -> string (string_of_int v ^ "px")
-	 | `PCT v -> string (string_of_int v ^ "%")
-      )
-      
+
+  method z_index : int =
+    sty >>> get "zIndex" >>> as_int
+  method set_z_index (z : int) : unit =
+    sty >>> set "zIndex" (int z)
+
+  method background_color : color =
+    sty >>> get "background" >>> as_string
+  method set_background_color (c : color) : unit =
+    sty >>> set "background" (string c)
+
 end
 
 let style obj =
