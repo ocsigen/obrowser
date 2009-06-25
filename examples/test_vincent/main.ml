8618944a250f04cc9707aae0c020b8204d7289a6
Benjamin.Canou <Benjamin.Canou@gmail.com>
2009-06-25 13:27:45 +0200

    remove old test
    Ignore-this: e2c3cc38989394fa7ecc2e6af107fd01
    
    darcs-hash:20090625112745-2ddb7-a584ca895c882ff3a308d3f68f0e7704d83bfb01

--- examples/test_vincent/main.ml
+++
@@ -1,7 +0,0 @@
-open Js
-open Table
-
-let _ =
-  register_closure
-    132
-    (fun (s, i) -> alert ("test : " ^ s ^ ", " ^ string_of_int i))
--- examples/test_vincent/table_dummy.c
+++
@@ -1,2 +0,0 @@
-int caml_register_closure = 0;
-int caml_get_closure_arg = 0;
