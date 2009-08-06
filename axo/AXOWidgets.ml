e12cb044dc3e8aee733c6bb9010e3105f1136a5c
raphlalou [AT] hotmail [DOT] fr <none>
2009-08-06 19:58:13 +0200

    Uniformisation
    Ignore-this: 1242d70ac9cfa1e11ffbc51302280b3a
    
    darcs-hash:20090806175813-58cab-c910c65321480d5e583b847c5e985afbdb9001b8

--- axo/AXOWidgets.ml
+++ axo/AXOWidgets.ml
@@ -19,6 +19,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *)
 
+
 (** This module allows easy creation of "graphical" widgets *)
 
 (** Each widget kind is organised with :
