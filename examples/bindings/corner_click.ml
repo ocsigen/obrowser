a1e5f66a35006c094b3102be02edaece05a5f418
Benjamin.Canou <Benjamin.Canou@gmail.com>
2010-02-21 18:28:12 +0100

    ffi example
    Ignore-this: 8064142a8c357ed135ddd8d033ba7809
    
    darcs-hash:20100221172812-2ddb7-fc279b03b92f23804dd20def3cccd08cc99e92cd

---
+++ examples/bindings/corner_click.ml
@@ -0,0 +1,2 @@
+external corner_click : unit -> (int * int) = "corner_click"
+external corner_click_callback : (int -> int -> 'a) -> 'a = "corner_click_callback"
---
+++ examples/bindings/corner_click_stubs.c
@@ -0,0 +1,2 @@
+int corner_click = 0;
+int corner_click_callback = 0;
